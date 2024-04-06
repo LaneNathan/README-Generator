746cd1dd55e12290b153a40dda28b9e8209f2ee7
Nathan Lane <lanenathan214@gmail.com>
2024-04-05 21:54:51 -0400

    wrote functions to write the readme and initialize the app

--- Develop/index.js
+++ Develop/index.js
@@ -64,10 +64,17 @@ const questions = [
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    return fs.writeToFile(path.join(process.cwd(),fileName),data);
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions).then((inquirerResponses) =>{
+        console.log('Generating README...');
+        writeToFile('README.md', generateMarkdown({...inquirerResponses}));
+    });
+}
 
 // Function call to initialize app
 init();
