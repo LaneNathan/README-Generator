2826936377e6080275522250908302d965172ccb
Nathan Lane <lanenathan214@gmail.com>
2024-04-06 22:18:50 -0400

    wrote function for getting license badge, returning said badge and adding license to section

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,14 +1,31 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function LicenseBadge(license) {
+  if(license !== 'None'){
+    return `![Static Badge](https://img.shields.io/badge/license-${license}-green.svg)`;
+  }
+  return '';
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function LicenseLink(license) {
+  if(license !== 'None') {
+    return `\n* [License](#license)\n`;
+  }
+  return '';
+  }
+
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function LicenseSection(license) {if(license !== 'None') {
+  return  `## License
+
+  This project is licensed by ${license}.`;
+}
+return '';
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
